6e4e3bcac50b920c855131ebadce3831ede5343b
Angel D. Munoz <scythes.lol@gmail.com>
2021-11-26 07:02:40 -0600

    add queries with FullName and name

--- Database.fs
+++ Database.fs
@@ -15,6 +15,9 @@ module Database =
         repo.EnsureIndex(fun clamRepo -> clamRepo.fullName)
         |> ignore
 
+        repo.EnsureIndex(fun clamRepo -> clamRepo.name)
+        |> ignore
+
         repo
 
     let closeDatabase () = db.Value.Dispose()
@@ -31,21 +34,50 @@ module Database =
             | None -> return! None
         }
 
+    /// <summary>
+    /// Checks if the the repository with given a name in the form of
+    /// Username/Repository
+    /// exists
+    /// <param name="fullName">Full name of the template in the Username/Repository scheme</param>
+    /// </summary>
+    let existsByFullName fullName =
+        clamRepos()
+            .Exists(fun clamRepo -> clamRepo.fullName = fullName)
+
+    /// <summary>
+    /// Checks if the repository exists given a simple name 
+    /// <param name="name">Simple name of the repository (not including the GitHub owner)</param>
+    /// </summary>
     let existsByName name =
         clamRepos()
-            .Exists(fun clamRepo -> clamRepo.fullName = name)
+            .Exists(fun clamRepo -> clamRepo.name = name)
 
+    /// <summary>
+    /// Finds a repository using the name of the repository.
+    /// <param name="name">Simple name of the repository (not including the GitHub owner)</param>
+    /// </summary>
     let findByName name =
         clamRepos().FindOne(fun repo -> repo.name = name) :> obj
         |> Option.ofObj
         |> Option.map (fun o -> o :?> ClamRepo)
 
+    /// <summary>
+    /// Finds a repository using the full name of the repository (ex. Username/Repository)
+    /// <param name="fullName">Full name of the repository including the GitHub owner</param>
+    /// </summary>
+    let findByFullName fullName =
+        clamRepos()
+            .FindOne(fun repo -> repo.fullName = fullName)
+        :> obj
+        |> Option.ofObj
+        |> Option.map (fun o -> o :?> ClamRepo)
 
     let updateByName name =
         match findByName name with
         | Some repo ->
             let repo =
-                { repo with updatedAt = Nullable(DateTime.Now) }
+                { repo with
+                      updatedAt = Nullable(DateTime.Now) }
 
             clamRepos().Update(repo)
         | None -> false
@@ -56,7 +88,7 @@ module Database =
             return! updateByName repo.fullName
         }
 
-    let deleteByName name =
-        match findByName name with
+    let deleteByFullName fullName =
+        match findByFullName fullName with
         | Some repo -> clamRepos().Delete(BsonValue(repo._id))
         | None -> false
